5053162d11cf07527a8d6f5c24d6b11bad07d61e
rdz.maria.laura <rdz.maria.laura@5832eb17-da4b-0410-be42-497838e67f70>
2008-05-19 00:28:20 +0000

    Yo pongo path y extension

--- Glog/glog.sh
+++ Glog/glog.sh
@@ -1,37 +1,39 @@
-#$1 = Nombre de archivo de log
+#$1 = Nombre de archivo de log (sin extensión)
 #$2 = Mensaje a loggear
 #$3 = Comando que me invoca. Si tiene el .sh no importa, ya que se lo saco.
 
-LOGSIZE=6 #Solo para probarlo hasta que no tenga bien setteado esto
+LOGDIR=./
+LOGSIZE=6
+LOGEXT=.log
 
-if [ -d $1 ] #Veo si es un directorio
+ARCHIVO_LOG=$LOGDIR/$1$LOGEXT
+
+if [ -d $ARCHIVO_LOG ] #Veo si es un directorio
 then
 	echo "El archivo donde desea escribir es un directorio"
 	exit 1
-elif [ ! -e `dirname $1` ]
+elif [ ! -e `dirname $ARCHIVO_LOG` ]
 then
 	echo "Path del archivo donde desea escribir invalido"
 	exit 2
 else
-	# Busco el nombre del proceso que invoco el log.
-	#cmd=$(ps -p $PPID -o "%a" | sed -n 's/^.*\.\/\([^.]*\)\.sh.*$/\1/p')
 	comando=`echo $3 | sed 's/\.sh$//'` #Le saco el .sh si es que lo tiene
-	mensaje="`date` - `basename $comando` - `whoami` - $2"
-	echo $mensaje >> $1
+	mensaje="`date +%D\ %T` - `basename $comando` - `whoami` - $2"
+	echo $mensaje >> $ARCHIVO_LOG
 
 	if [ "$LOGSIZE" != "" ]
 	then
-		if [ `expr $(stat -c%s "$1") / 1024` -ge $LOGSIZE ]
+		if [ `expr $(stat -c%s "$ARCHIVO_LOG") / 1024` -ge $LOGSIZE ]
 		then
 			IFSOriginal=$IFS
 			IFS=$'\t\n ' #IFS default, lo setteo por las dudas que quien me invoca lo tenga cambiado
-			cant_lineas=(`wc -l $1`)
+			cant_lineas=(`wc -l $ARCHIVO_LOG`)
 			IFS=$IFSOriginal
 			if [ $cant_lineas -gt 70 ]
 			then
 				ultima_linea=$(expr $cant_lineas - 70)
-				sed "1,$ultima_linea d" <$1 >$1.temp
-				mv $1.temp $1
+				sed "1,$ultima_linea d" <$ARCHIVO_LOG >$ARCHIVO_LOG.temp
+				mv $ARCHIVO_LOG.temp $ARCHIVO_LOG
 			fi
 		fi
 	fi
