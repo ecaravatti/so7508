648bfdea91f0ddb778a12f2834295ad203057c1b
rdz.maria.laura <rdz.maria.laura@5832eb17-da4b-0410-be42-497838e67f70>
2008-05-19 04:18:58 +0000

    Agregue comillas por todos lados

--- trunk/Glog/glog.sh
+++ trunk/Glog/glog.sh
@@ -9,19 +9,19 @@ then
 	LOGDIR="./"
 fi
 
-ARCHIVO_LOG=$LOGDIR/$1$LOGEXT
+ARCHIVO_LOG="$LOGDIR"/"$1""$LOGEXT"
 
-if [ -d $ARCHIVO_LOG ] #Veo si es un directorio
+if [ -d "$ARCHIVO_LOG" ] #Veo si es un directorio
 then
 	echo "El archivo donde desea escribir es un directorio"
 	exit 1
-elif [ ! -e `dirname $ARCHIVO_LOG` ]
+elif [ ! -e `dirname "$ARCHIVO_LOG"` ]
 then
 	echo "Path del archivo donde desea escribir invalido"
 	exit 2
 else
-	comando=`echo $3 | sed 's/\.sh$//'` #Le saco el .sh si es que lo tiene
-	mensaje="`date +%D\ %T` - `basename $comando` - `whoami` - $2"
+	comando=`echo "$3" | sed 's/\.sh$//'` #Le saco el .sh si es que lo tiene
+	mensaje="`date +%D\ %T` - `basename "$comando"` - `whoami` - "$2""
 	echo -e $mensaje >> $ARCHIVO_LOG
 
 	if [ "$LOGSIZE" != "" ]
@@ -30,13 +30,13 @@ else
 		then
 			IFSOriginal=$IFS
 			IFS=$'\t\n ' #IFS default, lo setteo por las dudas que quien me invoca lo tenga cambiado
-			cant_lineas=(`wc -l $ARCHIVO_LOG`)
+			cant_lineas=(`wc -l "$ARCHIVO_LOG"`)
 			IFS=$IFSOriginal
 			if [ $cant_lineas -gt 70 ]
 			then
 				ultima_linea=$(expr $cant_lineas - 70)
-				sed "1,$ultima_linea d" <$ARCHIVO_LOG >$ARCHIVO_LOG.temp
-				mv $ARCHIVO_LOG.temp $ARCHIVO_LOG
+				sed "1,$ultima_linea d" <"$ARCHIVO_LOG" >"$ARCHIVO_LOG".temp
+				mv "$ARCHIVO_LOG".temp "$ARCHIVO_LOG"
 			fi
 		fi
 	fi
