9da21f72d54b606acbbc69909452e7bb9d4c009b
dpisaturo <dpisaturo@5832eb17-da4b-0410-be42-497838e67f70>
2008-05-22 17:26:06 +0000

    modifiqué ginsta para que cree la nueva estructura de directorios que usa galida subo el instructivo para instalar la aplicación

--- start.sh
+++ start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 GRUPO="$HOME/Desktop/grupo03"
-PRUEBA="$HOME/Desktop/prueba"
+INSTALL="$HOME/Desktop/install"
 REPO="$HOME/Documentos/Facu/Sistemas Operativos/TPSO1C08"
 
 rm -r -f $GRUPO
-rm -r -f $PRUEBA
+rm -r -f $INSTALL
 mkdir $GRUPO
-mkdir -p $PRUEBA/tests
-cp "$REPO/Galida/galida.sh" $PRUEBA
-cp "$REPO/Gemoni/gemoni.sh" $PRUEBA
-cp "$REPO/Ginsta/ginsta.sh" $PRUEBA
-cp "$REPO/Glog/glog.sh" $PRUEBA
-cp "$REPO/Gontro/gontro.pl" $PRUEBA
-cp "$REPO/Gontro/gontrosub.pm" $PRUEBA
-cp "$REPO/Mover/mover.sh" $PRUEBA
-cp -r "$REPO/Tests/arridir/tests a entregar/invalidos/." $PRUEBA/tests/arridir
-cp -r "$REPO/Tests/arridir/tests a entregar/validos/." $PRUEBA/tests/arridir
-cp -r "$REPO/Tests/arridir/tests a entregar/validos/los que deberian procesar el periodo xq sus reg estan bien/." $PRUEBA/tests/arridir
-cp -r "$REPO/Tests/confdir" $PRUEBA/tests
-rm -r -f "$PRUEBA/tests/arridir/los que deberian procesar el periodo xq sus reg estan bien"
+mkdir -p $INSTALL/tests
+cp "$REPO/Galida/galida.sh" $INSTALL
+cp "$REPO/Gemoni/gemoni.sh" $INSTALL
+cp "$REPO/Ginsta/ginsta.sh" $INSTALL
+cp "$REPO/Glog/glog.sh" $INSTALL
+cp "$REPO/Gontro/gontro.pl" $INSTALL
+cp "$REPO/Gontro/gontrosub.pm" $INSTALL
+cp "$REPO/Mover/mover.sh" $INSTALL
+cp -r "$REPO/Tests/arridir/tests a entregar/invalidos/." $INSTALL/tests/arridir
+cp -r "$REPO/Tests/arridir/tests a entregar/validos/." $INSTALL/tests/arridir
+cp -r "$REPO/Tests/arridir/tests a entregar/validos/los que deberian procesar el periodo xq sus reg estan bien/." $INSTALL/tests/arridir
+cp -r "$REPO/Tests/confdir" $INSTALL/tests
+rm -r -f "$INSTALL/tests/arridir/los que deberian procesar el periodo xq sus reg estan bien"
